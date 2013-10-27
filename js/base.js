73e7ab78e5772468fd25f8a4485c2073edb7438e
nenodias <nenodias@linux-nb3m.site>
2013-10-26 22:07:35 -0200

    Alterações e funções dos botões

--- js/base.js
+++ js/base.js
@@ -1,5 +1,5 @@
     
-var _mediaId = "#mediaContainer" 
+var _mediaId = "#mediaContainer";
 
 var $mute = $("#mute");
 var $unmute = $("#unmute");
@@ -9,21 +9,25 @@ var $stop = $("#stop");
 var $back = $("#back");
 var $next = $("#next");
 
+var $labeMusica = $(".label-musica");
+var $barraMusica = $(".barra-musica");
+
 var _esconde = "hide";
+var _activa = "active";
 
 var _optionsPlayer = {
         swfPath: "/lib/jQuery.jPlayer.2.4.0jQuery.jPlayer.2.4.0/",
-        supplied: "m4v, ogv",
-        cssSelectorAncestor: "",
+        supplied: "mp3",
+        cssSelectorAncestor: "#player",
         cssSelector: {
-            play: "#play",
-            pause: "#pause",
-            stop: "#stop",
-            mute: "#mute",
-            unmute: "#unmute",
-            currentTime: "#currentTime",
-            duration: "#duration",
-            seekBar: '.progress-striped',
+            play: ".play",
+            pause: ".pause",
+            stop: ".stop",
+            mute: ".mute",
+            unmute: ".unmute",
+            currentTime: ".current-time",
+            duration: ".duration",
+            seekBar: '.barra-musica',
             playBar: '.progress-bar'
           },
         size: {
@@ -33,23 +37,36 @@ var _optionsPlayer = {
 };
 /**
 
-	Funções e Parâmetros
+    Funções e Parâmetros
 
 **/
-$mute.on("click", function(){
-	$(this).addClass(_esconde);
-	$unmute.removeClass(_esconde)
-});
-$unmute.on("click", function(){
-	$(this).addClass(_esconde);
-	$mute.removeClass(_esconde)
-});
+var escondeEMostra = function ($obj1, $obj2){
+    $obj1.addClass(_esconde);
+    $obj2.removeClass(_esconde);
+}
 
+var escondePlayEMostraPause = function(){
+    escondeEMostra($play,$pause);
+    $barraMusica.addClass(_activa);
+}
+var escondePauseEMostraPlay = function(){
+    escondeEMostra($pause,$play);
+    $barraMusica.removeClass(_activa);
+}
+
+$play.on("click", escondePlayEMostraPause);
+$mute.on("click",  escondeEMostra($(this),$unmute));
+$unmute.on("click", escondeEMostra($(this),$mute));
+/*
 $(_mediaId).jPlayer();
+*/
 
-    $(_mediaId).bind($.jPlayer.event.pause, function(event) {
-        $(this).jPlayer("clearMedia");
-        $(this).jPlayer("setMedia", {
-                mp3: server
-        });
-    });
+var voltarPlayEPause = function(event) {
+    escondePauseEMostraPlay();
+};
+
+$(_mediaId).bind($.jPlayer.event.pause, voltarPlayEPause);
+$(_mediaId).bind($.jPlayer.event.ended, voltarPlayEPause);
+// $(_mediaId).bind($.jPlayer.event.stop, function(event) { 
+//      escondePauseEMostraPlay();
+// });
