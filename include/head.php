4df899e74d2bd2d351df88118f3dd3c67e679bf6
nenodias <horacio.dias92@gmail.com>
2023-05-23 13:42:41 -0300

    Update project using jquery 3.7.0

--- include/head.php
+++ include/head.php
@@ -2,6 +2,6 @@
 <link rel="stylesheet" href="lib/bootstrap-3.0.0/css/bootstrap-theme.min.css" />
 <link rel="stylesheet" href="lib/bootstrap-3.0.0/css/bootstrap.min.css" />
 <link rel="stylesheet" href="css/base.css" />
-<script type="text/javascript" src="lib/jquery-2.0.3.min.js"></script>
+<script type="text/javascript" src="lib/jquery-3.7.0.min.js"></script>
 <script type="text/javascript" src="lib/bootstrap-3.0.0/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="lib/jQuery.jPlayer.2.4.0/jquery.jplayer.min.js"></script>
